363dd832ec13e60256e2549daa5ef7ada9599c1d
umberto <umberto@172.16.63.134>
2019-05-17 10:57:42 +0200

    s

---
+++ StudioMedico/GUI/FrameLogin.java
@@ -0,0 +1,18 @@
+package Utenti;
+
+import javax.swing.JTextField;
+
+import Utility.Frame;
+
+public class FrameLogin extends Frame {
+	
+	Frame() {
+		// 2 edittext e 2 button
+		
+		JTextField email = new JTextField();
+		JTextField email = new JTextField();
+		
+		
+		setVisible(true);
+	}
+}
