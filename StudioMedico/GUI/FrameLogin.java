7f341c09d585182f9c9b28bc642c2b61b51e2ef5
umberto <umberto@172.16.63.134>
2019-05-17 11:09:35 +0200

    s

--- StudioMedico/GUI/FrameLogin.java
+++ StudioMedico/GUI/FrameLogin.java
@@ -1,17 +1,27 @@
 package Utenti;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import Utility.Frame;
 
 public class FrameLogin extends Frame {
 	
-	Frame() {
+	FrameLogin() {
 		// 2 edittext e 2 button
+		Frame();
 		
-		JTextField email = new JTextField();
-		JTextField email = new JTextField();
+		JTextField emailTextField = new JTextField();
+		JTextField passwordTextField = new JTextField();
 		
+		JLabel emailLabel = new JLabel();
+		JLabel passwordLabel = new JLabel();
+		
+		JButton loginButton = new JButton();
+		JButton signUpButton = new JButton();
+		
+		get
 		
 		setVisible(true);
 	}
