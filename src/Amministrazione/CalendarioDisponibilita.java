50d99fc693202b784d81a7ccea7c419e62929ef6
Marco <Marco@Marco-PC.station>
2019-05-18 10:14:44 +0200

    get e set CalDisp

--- src/Amministrazione/CalendarioDisponibilita.java
+++ src/Amministrazione/CalendarioDisponibilita.java
@@ -4,23 +4,48 @@ import java.util.ArrayList;
 import Utenti.Medico;
 
 public class CalendarioDisponibilita {
-
 	private ArrayList<Disponibilita> orari;
-
 	private int anno;
-
 	private boolean[] approvato;
-
 	private Medico medico;
+	
+	public ArrayList<Disponibilita> getOrari() {
+		return orari;
+	}
 
-	private Medico medico;
+	public void setOrari(ArrayList<Disponibilita> orari) {
+		this.orari = orari;
+	}
 
-	public void addDiponibilita(Disponibilita disponibilita) {
+	public int getAnno() {
+		return anno;
+	}
+
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+	public boolean[] getApprovato() {
+		return approvato;
+	}
+
+	public void setApprovato(boolean[] approvato) {
+		this.approvato = approvato;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public void addDisponibilita(Disponibilita disponibilita) {
 
 	}
 
 	public boolean removeDisponibilita(Disponibilita disponibilita) {
 		return false;
 	}
-
 }
