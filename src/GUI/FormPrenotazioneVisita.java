95743237e7f7958b20073ac46f3d89657f4a5baf
UmbertoCovino <50734619+UmbertoCovino@users.noreply.github.com>
2019-05-19 21:57:38 +0200

    Update FormPrenotazioneVisita.java

--- src/GUI/FormPrenotazioneVisita.java
+++ src/GUI/FormPrenotazioneVisita.java
@@ -1,3 +1,13 @@
+/* 2
+Form
+Label JComboBox per tipologia visita
+" per medico
+Label JCalendar per calendario (per il gg)
+Label JComboBox per e ora
+Button per annulla
+" per conferma
+*/
+
 package GUI;
 
 public class FormPrenotazioneVisita {
