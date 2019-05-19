14148fb1024dcea7f7261e56b6b6ae41d5c434e5
UmbertoCovino <50734619+UmbertoCovino@users.noreply.github.com>
2019-05-19 21:41:48 +0200

    Update FormRegistrazionePaziente.java

--- src/GUI/FormRegistrazionePaziente.java
+++ src/GUI/FormRegistrazionePaziente.java
@@ -1,3 +1,15 @@
+/* 1
+Form
+Label TextField per nome
+" per cognome
+" per email
+" per password
+" per codice fiscale
+Button per conferma
+Button per annullare che forward su exit
+
+*/
+
 package GUI;
 
 public class FormRegistrazionePaziente {
