8325bf315011bf4a8a7b130ce418ea611ccbed5d
umberto <umberto@air-di-umberto>
2019-06-03 17:14:37 +0200

    fesserie

--- studio_medico_population.sql
+++ studio_medico_population.sql
@@ -6,30 +6,30 @@ values ('CF1', 'Marco', 'Cor', 'mc@sm.com', 'mcsm');
 
 insert into specializzazioni
 values ('cardiologo'),
-		('dentista');
+	   ('dentista');
 
 insert into medici (nome, cognome, email, password, nome_specializzazione)
 values ('doc', 'cuore', 'doc1@sm.com', 'doc1', 'cardiologo'),
-		('doc', 'denti', 'doc2@sm.com', 'doc2', 'dentista');
+	   ('doc', 'denti', 'doc2@sm.com', 'doc2', 'dentista');
 
 insert into calendario_disponibilita (anno, codice_medico)
 values (2019, 1),
-		(2019, 2);
+	   (2019, 2);
 
 insert into disponibilita (giorno, ora_inizio, ora_fine, max_num_visite, presenza, id_calendario_disponibilita)
 values ('2019-07-03', '16:00', '19:00', 6, 0, 1),
-		('2019-07-05', '17:00', '20:00', 6, 0, 1),
-		('2019-07-07', '16:00', '19:00', 6, 0, 1),
-		('2019-07-04', '9:00', '12:00', 6, 0, 2),
-		('2019-07-06', '9:00', '12:00', 6, 0, 2);
+	   ('2019-07-05', '17:00', '20:00', 6, 0, 1),
+	   ('2019-07-07', '16:00', '19:00', 6, 0, 1),
+	   ('2019-07-04', '9:00', '12:00', 6, 0, 2),
+	   ('2019-07-06', '9:00', '12:00', 6, 0, 2);
 
 insert into tipologie_visite (nome, prezzo_fisso, costo_manodopera, costo_esercizio)
 values ('Elettrocardiogramma', '50.00', '5.00', '5.00'),
-		('Visita odontoiatrica generica', '85.00', '10.00', '5.00');
+	   ('Visita odontoiatrica generica', '85.00', '10.00', '5.00');
 
 insert into tipologie_visite_specializzazioni
 values (1, 'cardiologo'),
-		(2, 'dentista');
+	   (2, 'dentista');
 
 insert into prenotazioni (giorno, ora, id_tipologia_visita, codice_medico, codice_fiscale_paziente)
 values ('2019-07-05', '16:30', 1, 1, 'CF1');
