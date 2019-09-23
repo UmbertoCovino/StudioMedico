c02cd084db64afb9893412c1e34498ceece97ab8
umberto <umberto@172.16.62.92>
2019-09-23 11:57:29 +0200

    Umberto

--- studio_medico_population.sql
+++ studio_medico_population.sql
@@ -12,6 +12,7 @@ values ('cardiologo'),
 
 insert into medici (nome, cognome, email, password, nome_specializzazione)
 values ('Mario', 'Rossi', 'mr@sm.com', 'mr', 'cardiologo'),
+	   ('Maria', 'Sossi', 'ms@sm.com', 'ms', 'cardiologo'),
 	   ('Antonio', 'Bianchi', 'ab@sm.com', 'ab', 'dentista'),
 	   ('Giuseppe', 'Fermi', 'gf@sm.com', 'gf', 'ortopedico');
 
@@ -27,7 +28,18 @@ values ('2019-11-03', '16:00', '19:00', 6, 0, 1),
 	   ('2019-11-04', '9:00', '12:00', 6, 0, 2),
 	   ('2019-11-06', '9:00', '12:00', 6, 0, 2),
 	   ('2019-11-02', '16:00', '19:00', 6, 0, 3),
-	   ('2019-11-09', '9:00', '12:00', 6, 0, 3);
+	   ('2019-11-09', '9:00', '12:00', 6, 0, 3),
+	   ('2019-09-03', '16:00', '19:00', 6, 0, 1),
+	   ('2019-09-05', '17:00', '20:00', 6, 0, 1),
+	   ('2019-09-07', '16:00', '19:00', 6, 0, 1),
+	   ('2019-09-04', '9:00', '12:00', 6, 0, 2),
+	   ('2019-09-06', '9:00', '12:00', 6, 0, 2),
+	   ('2019-09-02', '16:00', '19:00', 6, 0, 3),
+	   ('2019-09-09', '9:00', '12:00', 6, 0, 3),
+	   -- qui le diponibilità odierne (CAMBIARE GIORNO E MESE SE NECESSARIO)
+	   ('2019-09-23', '9:00', '12:00', 6, 0, 1),
+	   ('2019-09-23', '16:00', '19:00', 6, 0, 2),
+	   ('2019-09-23', '9:00', '12:00', 6, 0, 3);
 
 insert into tipologie_visite (nome, prezzo_fisso, costo_manodopera, costo_esercizio)
 values ('Elettrocardiogramma', '50.00', '5.00', '5.00'),
@@ -43,7 +55,9 @@ values (1, 'cardiologo'),
 	   (4, 'cardiologo');
 
 insert into prenotazioni (giorno, ora, id_tipologia_visita, codice_medico, codice_fiscale_paziente)
-values ('2019-05-05', '17:30', 1, 1, 'CVNMRT96A18A783J');
+values ('2019-05-05', '17:30', 1, 1, 'CVNMRT96A18A783J'),
+	   -- qui le prenotazioni odierne (CAMBIARE GIORNO E MESE SE NECESSARIO)
+	   ('2019-09-23', '17:30', 1, 1, 'CVNMRT96A18A783J');
 
 insert into visite (diagnosi, terapia, id_prenotazione)
 values ('Elettrocardiogramma negativo', 'Nessuna', 1);
@@ -53,6 +67,3 @@ values ('60.00', 1, 'CVNMRT96A18A783J');
 
 insert into pagamenti (data, metodo_pagamento, id_fattura)
 values ('2019-05-15', 'Bonifico bancario', 1);
-
-insert into 
-values ('', '', '', '', '');
